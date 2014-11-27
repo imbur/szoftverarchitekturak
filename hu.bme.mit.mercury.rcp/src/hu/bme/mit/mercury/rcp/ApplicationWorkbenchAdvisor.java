f1685a604cba596e7a14f18b5ab2b9a5884e7e94
Marton Bur <marton.bur@gmail.com>
2014-11-27 23:14:56 +0100

    Initial commit of the project for the RCP application.

---
+++ hu.bme.mit.mercury.rcp/src/hu/bme/mit/mercury/rcp/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package hu.bme.mit.mercury.rcp;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "hu.bme.mit.mercury.rcp.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
