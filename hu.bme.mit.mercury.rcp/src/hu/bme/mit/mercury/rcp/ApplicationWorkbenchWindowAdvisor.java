d47034794414fcdcc9dfd9de13ab88b3ebabe129
Marton Bur <marton.bur@gmail.com>
2014-11-28 01:09:47 +0100

    Removes unnecessary UI contributions

--- hu.bme.mit.mercury.rcp/src/hu/bme/mit/mercury/rcp/ApplicationWorkbenchWindowAdvisor.java
+++ hu.bme.mit.mercury.rcp/src/hu/bme/mit/mercury/rcp/ApplicationWorkbenchWindowAdvisor.java
@@ -32,5 +32,18 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
     @Override
     public void postWindowOpen() {
         super.postWindowOpen();
+        
+        IWorkbenchWindow[] windows = PlatformUI.getWorkbench().getWorkbenchWindows();
+        for (int i = 0; i < windows.length; ++i) {
+            IWorkbenchPage page = windows[i].getActivePage();
+            if (page != null) {
+                page.hideActionSet("org.eclipse.ui.actionSet.openFiles");
+                page.hideActionSet("org.eclipse.ui.edit.text.actionSet.convertLineDelimitersTo");
+                page.hideActionSet("org.eclipse.search.searchActionSet");
+                page.hideActionSet("org.eclipse.ui.externaltools.ExternalToolsSet");
+                page.hideActionSet("org.eclipse.ui.edit.text.actionSet.annotationNavigation");
+                page.hideActionSet("org.eclipse.ui.edit.text.actionSet.navigation");
+            }
+        }
     }
 }
